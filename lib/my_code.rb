f1cbba6f90f8e6ad34fd69145c25b57bd6b1d671
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 17:05:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,42 @@
 # Your Code Here
+# My Code here....
+
+def map(array)
+
+  i = 0
+  result = []
+
+  until  i == array.length do
+
+    result.push(yield(array[i]))
+    i += 1
+
+  end
+
+p result
+
+end
+
+#using yield to pass an argument
+
+def reduce(array, starting_point = nil)
+
+  if starting_point
+    i = 0
+    total = starting_point
+  else
+    i = 1
+    total = array[0]
+  end
+
+   until  i == array.length do
+
+     total = yield(total, array[i])
+
+     i += 1
+
+   end
+
+   p total
+
+end
